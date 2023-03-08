95f3632f1b0fcb1cbbfb6ff27d6912006751ad57
Kurupeco <kurupecoheh@gmail.com>
2023-03-08 16:55:33 -0500

    created test motors

--- src/main/java/frc/robot/subsystems/drivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain.java
@@ -1,9 +1,23 @@
-import frc.robot.subsystems;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import frc.robot.Constants;
-import edu.wpi.first.wpilibj.XboxController;
+package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-public class drivetrain {
-    
+public class MyMotorSubsystem extends SubsystemBase {
+  private final CANSparkMax m_motor;
+
+  public MyMotorSubsystem() {
+    kmotor = new CANSparkMax(Constants.kTalonFXID, MotorType.kBrushless);
+  }
+
+  public void setSpeed(double speed) {
+    kmotor.set(speed);
+  }
+
+  public void stop() {
+    kmotor.set(0);
+  }
 }
+
+
