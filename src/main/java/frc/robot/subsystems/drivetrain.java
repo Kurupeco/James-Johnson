c00daf554ff3db997973a83ee3e3cdfbff05acb1
Kurupeco <kurupecoheh@gmail.com>
2023-03-31 09:50:40 -0400

    added gearbox commands

--- src/main/java/frc/robot/subsystems/drivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain.java
@@ -1,16 +1,18 @@
 package frc.robot.subsystems;
+
 import frc.robot.lib.DriveModule;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.*;
 
-public class Drivetrain extends SubsystemBase {
+public class drivetrain extends SubsystemBase {
     /* Drivetrain subsystem responsible for the movement of the robot
      * contains only setup and basic control */
-    public Drivetrain() {}
+    public drivetrain() {}
 
     private DriveModule leftMotors = new DriveModule(DRIVETRAIN_LEFT_BACK_GEARBOX_VICTORSP, DRIVETRAIN_LEFT_FRONT_GEARBOX_VICTORSP); 
     private DriveModule rightMotors = new DriveModule(DRIVETRAIN_RIGHT_BACK_GEARBOX_VICTORSP, DRIVETRAIN_RIGHT_FRONT_GEARBOX_VICTORSP); 
 
+    
 @Override
     public void periodic() {
         // This method will be called once per scheduler run
@@ -21,3 +23,13 @@ public class Drivetrain extends SubsystemBase {
         // This method will be called once per scheduler run during simulation
     }
 }
+
+/*
+system.out.println ("fheggeegi");
+
+
+
+
+*/
+
+
